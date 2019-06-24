4c995fb2c0886923c92486941304367a032be834
Dominik <36657497+D0miH@users.noreply.github.com>
2019-06-24 09:49:54 +0200

    added main entry and types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 const { SMCNodeKit } = require('../build/Release/smcnodekit.node');
 
-const smc = new SMCNodeKit();
-smc.open();
-console.log('SMCNodeKit', smc.isOnAC());
-smc.close();
-module.exports = SMCNodeKit;
+export default SMCNodeKit;
