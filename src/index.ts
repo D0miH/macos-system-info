e8c961fd93763c96916ad2fa14403d5812390462
Dominik <36657497+D0miH@users.noreply.github.com>
2019-06-30 17:42:13 +0200

    added typescript class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,88 @@
 const binding = require('../binding/smc-node-kit.node');
 
-export const SMCNodeKit = binding.SMCNodeKit;
+export interface IKeyInfo {
+    type: string;
+    size: number;
+}
+
+export class SMCNodeKit {
+    private smcKit: any;
+
+    constructor() {
+        this.smcKit = new binding.SMCNodeKit();
+    }
+
+    /**
+     * Opens a new connection to the SMC.
+     */
+    public open() {
+        this.smcKit.open();
+    }
+
+    /**
+     * Closes the connection to the SMC.
+     */
+    public close() {
+        this.smcKit.close();
+    }
+
+    /**
+     * Returns information about the given SMC key. If the given key does not exist an exception is thrown.
+     * @param key   The SMC key
+     */
+    public getKeyInfo(key: string): IKeyInfo {
+        return this.smcKit.getKeyInfo(key);
+    }
+
+    /**
+     * Returns the current temperature of the cpu in degrees celcius.
+     */
+    public getCpuTemp(): number {
+        return this.smcKit.getCpuTemp();
+    }
+
+    /**
+     * Returns the number of fans of the machine.
+     */
+    public getFanCount(): number {
+        return this.smcKit.getFanCount();
+    }
+
+    /**
+     * Returns the minimum rpm of the fan.
+     * @param fanId The fan id
+     */
+    public getMinFanSpeed(fanId: number): number {
+        return this.smcKit.getMinFanSpeed(fanId);
+    }
+
+    /**
+     * Returns the maximum rpm of the fan.
+     * @param fanId The fan id
+     */
+    public getMaxFanSpeed(fanId: number): number {
+        return this.smcKit.getMaxFanSpeed(fanId);
+    }
+
+    /**
+     * Returns the current rpm of the fan.
+     * @param fanId The fan id
+     */
+    public getCurrentFanSpeed(fanId: number): number {
+        return this.smcKit.getCurrentFanSpeed(fanId);
+    }
+
+    /**
+     * Returns the number of batteries.
+     */
+    public getBatteryCount(): number {
+        return this.smcKit.getBatteryCount();
+    }
+
+    /**
+     * Returns true if the machine is on AC and returns false otherwise.
+     */
+    public isOnAc(): boolean {
+        return this.smcKit.isOnAc();
+    }
+}
